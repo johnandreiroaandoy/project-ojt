c02cd6e59ea94a5f31ba97ef4d4cd06ccb84e0df
John Andrei Roa Andoy <johnandreiroa.andoy@ccd.edu.ph>
2026-06-11 15:05:08 +0800

    App.jsx to merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header.jsx';
+import Header from  './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Router from './routes/router.jsx';
 import './index.css';
